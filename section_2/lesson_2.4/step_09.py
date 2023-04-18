606596c7e9cfec5ced394efbf98c2e49235d7356
Kustov Ilya <kusgent@yandex.ru>
2023-04-18 17:57:27 +0300

    Add step_09.py

---
+++ section_2/lesson_2.4/step_09.py
@@ -0,0 +1,6 @@
+# https://stepik.org/lesson/265079/step/9?auth=login&unit=246027
+
+num = int(input())
+
+print("Следующее за числом", num, "число:", num + 1)
+print("Для числа", num, "предыдущее число:", num - 1)
