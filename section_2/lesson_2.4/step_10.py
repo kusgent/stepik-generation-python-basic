b7a91215f7fe8f2f2fcff3cb145c27268fff12c4
Kustov Ilya <kusgent@yandex.ru>
2023-04-18 18:03:17 +0300

    Add step_10.py

---
+++ section_2/lesson_2.4/step_10.py
@@ -0,0 +1,8 @@
+# https://stepik.org/lesson/265079/step/10?auth=login&unit=246027
+
+monitor = int(input())
+system_unit = int(input())
+keyboard = int(input())
+mouse = int(input())
+
+print((monitor + system_unit + keyboard + mouse) * 3)
