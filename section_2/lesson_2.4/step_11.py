abeddc226240babbec23acd009c5b6b9056720bb
Kustov Ilya <kusgent@yandex.ru>
2023-04-18 18:06:00 +0300

    Add step_11.py

---
+++ section_2/lesson_2.4/step_11.py
@@ -0,0 +1,8 @@
+# https://stepik.org/lesson/265079/step/11?auth=login&unit=246027
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1, "+", num2, "=", num1 + num2)
+print(num1, "-", num2, "=", num1 - num2)
+print(num1, "*", num2, "=", num1 * num2)
