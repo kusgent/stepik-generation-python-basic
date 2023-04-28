a914a1eb26f65984e67e8c723f654eec16b98fa2
Kustov Ilya <kusgent@yandex.ru>
2023-04-28 12:02:30 +0300

    Add step_04.py

---
+++ section_4/lesson_4.1/step_04.py
@@ -0,0 +1,9 @@
+# https://stepik.org/lesson/265081/step/4?auth=login&unit=246029
+
+pass1 = input()
+pass2 = input()
+
+if pass1 == pass2:
+    print("Пароль принят")
+else:
+    print("Пароль не принят")
