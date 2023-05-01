2584e504dc21f3bde0ea3ad38707fdd88d9a542f
Kustov Ilya <kusgent@yandex.ru>
2023-05-01 12:25:00 +0300

    Add step_08.py

---
+++ section_4/lesson_4.1/step_08.py
@@ -0,0 +1,10 @@
+# https://stepik.org/lesson/265081/step/8?auth=login&unit=246029
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+if num2 - num1 == num3 - num2:
+    print("YES")
+else:
+    print("NO")
