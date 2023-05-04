43fbb1efa688169be572e2d30e135f44ff212df3
Kustov Ilya <kusgent@yandex.ru>
2023-05-04 14:37:26 +0300

    Add step_10.py

---
+++ section_4/lesson_4.2/step_10.py
@@ -0,0 +1,8 @@
+# https://stepik.org/lesson/265083/step/10?auth=login&unit=246031
+
+x = int(input())
+
+if x > -30 and x <= -2 or x > 7 and x <= 25:
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
