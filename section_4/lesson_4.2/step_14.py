98b2d74820b5b8a3115d6807b34c8ae17f52a699
Kustov Ilya <kusgent@yandex.ru>
2023-05-16 22:54:55 +0300

    Add step_14.py

---
+++ section_4/lesson_4.2/step_14.py
@@ -0,0 +1,11 @@
+# https://stepik.org/lesson/265083/step/14?auth=login&unit=246031
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a - c == 0 or b - d == 0:
+    print("YES")
+else:
+    print("NO")
