03afb196693304ed7cec1bb838cef71ba2f1ae48
Kustov Ilya <kusgent@yandex.ru>
2023-05-18 18:10:36 +0300

    Add step_15.py

---
+++ section_4/lesson_4.2/step_15.py
@@ -0,0 +1,11 @@
+# https://stepik.org/lesson/265083/step/15?auth=login&unit=246031
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if -1 <= a - c <= 1 and -1 <= b - d <= 1:
+    print("YES")
+else:
+    print("NO")
