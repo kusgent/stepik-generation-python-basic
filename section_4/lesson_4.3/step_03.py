e1e0decce3c51eaa610eaf23e7920189670d6a30
Kustov Ilya <kusgent@yandex.ru>
2023-05-22 14:54:28 +0300

    Add step_03.py

---
+++ section_4/lesson_4.3/step_03.py
@@ -0,0 +1,11 @@
+# https://stepik.org/lesson/265082/step/3?auth=login&unit=246030
+
+n = int(input())
+k = int(input())
+
+if n > k:
+    print("NO")
+elif k > n:
+    print("YES")
+else:
+    print("Don't know")
