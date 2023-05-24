c8df6470005bc8af502151b03b68d45003c08e5b
Kustov Ilya <kusgent@yandex.ru>
2023-05-24 22:05:30 +0300

    Add step_07.py

---
+++ section_4/lesson_4.3/step_07.py
@@ -0,0 +1,10 @@
+# https://stepik.org/lesson/265082/step/7?auth=login&unit=246030
+
+weight = int(input())
+
+if weight < 60:
+    print("Легкий вес")
+elif weight < 64:
+    print("Первый полусредний вес")
+elif weight < 69:
+    print("Полусредний вес")
